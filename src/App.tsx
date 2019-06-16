9150be30f2fc6f66c3d475c6aed1e28267ce79e6
Harry Chen <i@harrychen.xyz>
2019-06-16 22:53:52 +0200

    Add reset button to frontend
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/lab/Slider';
+import Button from '@material-ui/core/Button';
 
 import './App.css';
 
@@ -39,7 +40,9 @@ const App: React.FC = () => {
 
     // component did mount
     React.useEffect(() => {
-        fetch(SERVER_STATE_URL)
+        fetch(SERVER_STATE_URL, {
+            mode: 'no-cors'
+        })
             .then(r => r.json())
             .then((r: ClusterControl) => {
                 setState({
@@ -58,7 +61,7 @@ const App: React.FC = () => {
     }, []);
 
 
-    const stateChange = () => {
+    const stateChange = (reset: boolean) => {
 
         // disable changing
         setState({
@@ -68,8 +71,12 @@ const App: React.FC = () => {
         });
 
         fetch(SERVER_STATE_URL, {
-            body: JSON.stringify(state),
-            method: 'POST'
+            body: reset ? undefined : JSON.stringify(state),
+            method: reset ? 'DELETE' : 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            mode: 'no-cors'
         })
             .then(r => r.json())
             .then((r: ClusterControl) => {
@@ -88,110 +95,122 @@ const App: React.FC = () => {
             })
     };
 
+    const setNewState = () => stateChange(false);
+    const resetState = () => stateChange(true);
+
     return (
         <div className="App">
             <Typography variant="h4" className="title" gutterBottom>
                 {state.notice}
             </Typography>
             <div className='sliders'>
-            <div className="slider">
-                <Typography className="slider-title" gutterBottom>
-                    Fan
-                </Typography>
-                <Slider
-                    className="slider-content"
-                    orientation="vertical"
-                    defaultValue={initialState.fanLevel}
-                    value={state.fanLevel}
-                    min={0}
-                    max={100}
-                    step={1}
-                    disabled={!state.enabled}
-                    valueLabelDisplay="on"
-                    onChange={(e, n) => {
-                        setState({
-                            ...state,
-                            fanLevel: n as number
-                        })
-                    }}
-                    onMouseUp={stateChange}
-                    onTouchEnd={stateChange}
-                />
+                <div className="slider">
+                    <Typography className="slider-title" gutterBottom>
+                        Fan
+                    </Typography>
+                    <Slider
+                        className="slider-content"
+                        orientation="vertical"
+                        defaultValue={initialState.fanLevel}
+                        value={state.fanLevel}
+                        min={0}
+                        max={100}
+                        step={1}
+                        disabled={!state.enabled}
+                        valueLabelDisplay="on"
+                        onChange={(e, n) => {
+                            setState({
+                                ...state,
+                                fanLevel: n as number
+                            })
+                        }}
+                        onMouseUp={setNewState}
+                        onTouchEnd={setNewState}
+                    />
+                </div>
+                <div className="slider">
+                    <Typography className="slider-title" gutterBottom>
+                        CPU Power
+                    </Typography>
+                    <Slider
+                        className="slider-content"
+                        orientation="vertical"
+                        defaultValue={[initialState.cpuPowerAvg, initialState.cpuPowerMax]}
+                        value={[state.cpuPowerAvg, state.cpuPowerMax]}
+                        min={1}
+                        max={255}
+                        step={1}
+                        disabled={!state.enabled}
+                        valueLabelDisplay="on"
+                        onChange={(e, n) => {
+                            const numbers = (n as number[]).sort();
+                            setState({
+                                ...state,
+                                cpuPowerAvg: numbers[0],
+                                cpuPowerMax: numbers[1],
+                            })
+                        }}
+                        onMouseUp={setNewState}
+                        onTouchEnd={setNewState}
+                    />
+                </div>
+                <div className="slider">
+                    <Typography className="slider-title" gutterBottom>
+                        GPU Power
+                    </Typography>
+                    <Slider
+                        className="slider-content"
+                        orientation="vertical"
+                        defaultValue={initialState.gpuPower}
+                        value={state.gpuPower}
+                        min={100}
+                        max={250}
+                        disabled={!state.enabled}
+                        valueLabelDisplay="on"
+                        onChange={(e, n) => {
+                            setState({
+                                ...state,
+                                gpuPower: n as number,
+                            })
+                        }}
+                        onMouseUp={setNewState}
+                        onTouchEnd={setNewState}
+                    />
+                </div>
+                <div className="slider">
+                    <Typography className="slider-title" gutterBottom>
+                        GPU Frequency
+                    </Typography>
+                    <Slider
+                        className="slider-content"
+                        orientation="vertical"
+                        defaultValue={initialState.gpuFrequency}
+                        value={state.gpuFrequency}
+                        min={135}
+                        max={1350}
+                        step={7.5}
+                        disabled={!state.enabled}
+                        valueLabelDisplay="on"
+                        onChange={(e, n) => {
+                            setState({
+                                ...state,
+                                gpuFrequency: n as number,
+                            })
+                        }}
+                        onMouseUp={setNewState}
+                        onTouchEnd={setNewState}
+                    />
+                </div>
             </div>
-            <div className="slider">
-                <Typography className="slider-title" gutterBottom>
-                    CPU Power
-                </Typography>
-                <Slider
-                    className="slider-content"
-                    orientation="vertical"
-                    defaultValue={[initialState.cpuPowerAvg, initialState.cpuPowerMax]}
-                    value={[state.cpuPowerAvg, state.cpuPowerMax]}
-                    min={1}
-                    max={255}
-                    step={1}
-                    disabled={!state.enabled}
-                    valueLabelDisplay="on"
-                    onChange={(e, n) => {
-                        const numbers = (n as number[]).sort();
-                        setState({
-                            ...state,
-                            cpuPowerAvg: numbers[0],
-                            cpuPowerMax: numbers[1],
-                        })
-                    }}
-                    onMouseUp={stateChange}
-                    onTouchEnd={stateChange}
-                />
-            </div>
-            <div className="slider">
-                <Typography className="slider-title" gutterBottom>
-                    GPU Power
-                </Typography>
-                <Slider
-                    className="slider-content"
-                    orientation="vertical"
-                    defaultValue={initialState.gpuPower}
-                    value={state.gpuPower}
-                    min={100}
-                    max={250}
-                    disabled={!state.enabled}
-                    valueLabelDisplay="on"
-                    onChange={(e, n) => {
-                        setState({
-                            ...state,
-                            gpuPower: n as number,
-                        })
-                    }}
-                    onMouseUp={stateChange}
-                    onTouchEnd={stateChange}
-                />
-            </div>
-            <div className="slider">
-                <Typography className="slider-title" gutterBottom>
-                    GPU Frequency
-                </Typography>
-                <Slider
-                    className="slider-content"
-                    orientation="vertical"
-                    defaultValue={initialState.gpuFrequency}
-                    value={state.gpuFrequency}
-                    min={135}
-                    max={1350}
-                    step={7.5}
-                    disabled={!state.enabled}
-                    valueLabelDisplay="on"
-                    onChange={(e, n) => {
-                        setState({
-                            ...state,
-                            gpuFrequency: n as number,
-                        })
-                    }}
-                    onMouseUp={stateChange}
-                    onTouchEnd={stateChange}
-                />
-            </div>
-            </div>
+            <Button
+                className="reset-button"
+                color="secondary"
+                variant="contained"
+                disabled={!state.enabled}
+                onClick={resetState}
+            >
+                Clean Ass
+            </Button>
         </div>
     );
 };
